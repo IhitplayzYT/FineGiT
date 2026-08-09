ccf11ae558fd7d19672bea5b7880df91ca1e8c6c
Ihit Acharya <ihitexe@gmail.com>
2026-08-09 15:40:58 +0530

    Started adding if compiles

--- src/helper.rs
+++ src/helper.rs
@@ -1,11 +1,15 @@
 pub mod Helper{
-    use std::{collections::{HashMap, HashSet}, path::{Path, PathBuf}, process::{Command, exit}};
+    use std::{collections::{HashMap, HashSet}, fs::{self, Metadata}, ops::ControlFlow::Continue, path::{Path, PathBuf}, process::{Command, exit}};
 
+    use std::sync::RwLock;
+    use std::sync::LazyLock;
 
+    pub const DBG_STR: &str = "";
+    pub const OK:i32 = 0;
+    pub const ERR:i32 = -1;
 
-    const DBG_STR: &str = "";
-    const OK:i32 = 0;
-    const ERR:i32 = -1;
+    pub static f_meta: LazyLock<RwLock<Option<Metadata>>> = LazyLock::new(|| {RwLock::new(None)});
+
 
     #[derive(Debug,Clone)]
     pub struct FileSlice{
@@ -22,6 +26,7 @@ pub mod Helper{
         pub nested_gitignore: bool,
         pub graph: bool,
         pub conditions: Vec<String>,
+        pub condition_params: Vec<String>,
         pub local_commit_log: Option<PathBuf>,
         pub auto_repo_link: bool,
     }
@@ -38,7 +43,7 @@ pub mod Helper{
 
     impl CLI{
         pub fn new() -> Self{
-            Self {dbg: false ,files:HashSet::new(),splice_files:vec![],nested_gitignore:false,graph:false,conditions:vec![],local_commit_log:None,auto_repo_link: false}
+            Self {dbg: false ,files:HashSet::new(),splice_files:vec![],nested_gitignore:false,graph:false,conditions:vec![],local_commit_log:None,auto_repo_link: false,condition_params:vec![]}
         }
 
         pub fn Parse_Args(&mut self){
@@ -79,8 +84,10 @@ pub mod Helper{
                     }
                 } else if i == "--auto" || i == "-a" || i == "--Auto" || i == "-A" {
                     self.auto_repo_link = true
-                } else if i.starts_with("--if"){
+                } else if i.starts_with("--if") || i.starts_with("--IF"){
                     self.conditions.push((&i[i.find("[").unwrap()+1..i.find("]").unwrap()]).to_string());
+                }  else if i.starts_with("--if_then") || i.starts_with("--IF_THEN"){
+                    self.condition_params.push((&i[i.find("[").unwrap()+1..i.find("]").unwrap()]).to_string());
                 }else{
                     self.files.insert(i.to_string());
                 }
@@ -102,6 +109,71 @@ pub mod Helper{
     
 
         pub fn print_commit_history() {
+
+        }
+
+        pub fn process_flags(flags:u8,param: &str) {
+            if flags & (1 << 0) != 0{
+                if !Path::new(param).exists(){
+                    panic!("IF dependency not satisfied: {param} does not Exist!!");
+                }
+            }
+            
+            if flags & (1 << 1) != 0{
+                if Path::new(param).exists(){
+                    panic!("IF dependency not satisfied: {param} Exists!!");
+                }
+            }
+
+            if flags & (1 << 2) != 0{
+                if !Path::new(param).is_file(){
+                    panic!("IF dependency not satisfied: {param} is not a File!!");
+                }
+            }
+
+            if flags & (1 << 3) != 0{
+                if !Path::new(param).is_dir(){
+                    panic!("IF dependency not satisfied: {param} is not a Directory!!");
+                }
+            }
+            if flags & (1 << 4) != 0{
+                if !Path::new(param).exists(){
+                    fs::create_dir_all(param).unwrap();
+                }
+            }
+
+            if flags & (1 << 5) != 0{
+                if f_meta.try_read().unwrap().is_none(){
+                    let _ = f_meta.try_write().unwrap().insert(fs::metadata(param).unwrap());
+                }else{
+                    let f2_meta = fs::metadata(param).unwrap();
+                    if let Some(x) = &*f_meta.try_read().unwrap(){
+                       if f2_meta.len() == x.len() && ((f2_meta.is_file() && x.is_file()) || (f2_meta.is_dir() && x.is_dir())) {
+                       } else{
+                            panic!("IF dependency not satisfied: Files are not Similar!!");
+                       }
+                    }
+
+                }
+            }
+            
+            if flags & (1 << 6) != 0{
+                if f_meta.try_read().unwrap().is_none(){
+                    let _ = f_meta.try_write().unwrap().insert(fs::metadata(param).unwrap());
+                }else{
+                    let f2_meta = fs::metadata(param).unwrap();
+                    if let Some(x) = &*f_meta.try_read().unwrap(){
+                       if f2_meta.len() == x.len() && ((f2_meta.is_file() && x.is_file()) || (f2_meta.is_dir() && x.is_dir())) {
+                            panic!("IF dependency not satisfied: Files are Similar!!");
+                       }
+                    }
+
+                }
+            }
+
+
+
+
 
         }
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::process::exit;
+use std::{fs::Metadata, process::exit};
 
-use crate::helper::Helper::{CLI, get_username, print_commit_history};
+use crate::helper::Helper::{CLI, f_meta, get_username, print_commit_history, process_flags};
 
 mod helper;
 
@@ -29,6 +29,35 @@ fn main() {
     if clargs.nested_gitignore{
         ignore_files.append(&mut std::fs::read_to_string(".gitignore").unwrap().split("\n").collect());
     }
+
+    let mut flags = 0_u8;
+    for i in &clargs.conditions{
+        if flags != 0{
+            process_flags(flags,i);
+        }
+        if i.starts_with("-"){
+        flags = 0;
+        let iterator = i.chars().skip(1).enumerate();
+            for (idx,j) in iterator{
+                match j{
+                    'E' => {flags |= 1 << 0;},
+                    'N' => {flags |= 1 << 1;},
+                    'F' => {flags |= 1 << 2;},
+                    'D' => {flags |= 1 << 3;},
+                    'C' => {flags |= 1 << 4;},
+                    '!' => {flags |= 1 << 5;},
+                    '=' => {flags |= 1 << 6;},
+                    _ => {}
+                }
+
+            }
+                
+        
+        }
+
+
+    }
+
 
 
 
